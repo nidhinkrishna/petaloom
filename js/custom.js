16b31f911bc7c518beecd4d322ffe96bb0edd85d
Nidhin Krishna <nidhingopalank@gmail.com>
2024-02-04 11:56:25 +0530

    transitions added

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,15 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+//scroll reveal
+ScrollReveal({ 
+    reset: true,
+    distance: '30px',
+    duration: 1000,
+    delay: 150
+ });
+ ScrollReveal().reveal('.logo,.first-img,.nav-link', {origin:'top'});
+ ScrollReveal().reveal('.first-ltr,#products,.gal-img', {origin:'bottom'});
+
+ ScrollReveal().reveal('.why-us_main-title,.why-us-img,.projects h3',{origin:'left'});
+ ScrollReveal().reveal('.why-div-us,.contact h3', {origin:'right'});
